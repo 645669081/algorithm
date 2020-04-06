306f674880c7cb7ecfb9b0686c935f7eb2ecc702
lw <liang645669081@126.com>
2020-04-07 07:18:28 +0800

    普利姆算法暂时提交

---
+++ src/com/atguigu/prim/PrimAlgorithm.java
@@ -0,0 +1,15 @@
+package com.atguigu.prim;
+
+/**
+ * @Date 2020/4/7 6:54
+ * @Auther 梁伟
+ * @Description 使用普利姆算法获得最小生成树，解决修路问题。将顶点分为生成树内的和生成树外的，每次都选择一个外部顶点保证该外部顶点与内部顶点的权值
+ * 最小，找到后加入到内部。依次这样直到最后完成最小生成树的构建
+ *
+ * 在7个村子间修路，每条路的远近不同，在哪几个村子间修路可以使总的路的长度最短并使这些村子全部可以通行
+ */
+public class PrimAlgorithm {
+    public static void main(String[] args) {
+
+    }
+}
